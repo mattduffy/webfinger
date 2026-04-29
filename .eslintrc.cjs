e90f5ae6e29ad7ba409721f22d6412b7e9a4bb78
Matthew Duffy <mattduffy@gmail.com>
2026-04-28 20:17:40 -0700

    Update eslint packages and configuration.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,10 +1,36 @@
+const restrictedGlobals = require('eslint-restricted-globals')
+
 module.exports = {
+  settings: {
+    'import/resolver': {
+      exports: {},
+      node: {
+        extensions: ['.js', '.mjs', '.tx', 'tsx'],
+      },
+    },
+  },
+  globals: {
+    window: true,
+    document: true,
+    origin: true,
+    worker: true,
+  },
   env: {
     es2021: true,
     node: true,
+    browser: true,
+    worker: true,
   },
+  plugins: [
+  ],
   extends: 'airbnb-base',
   overrides: [
+    {
+      files: ['public/j/worker.js'],
+      rules: {
+        'no-restricted-globals': ['error', 'isFinite', 'isNaN'].concat(restrictedGlobals),
+      },
+    },
   ],
   parserOptions: {
     ecmaVersion: 'latest',
@@ -14,8 +40,10 @@ module.exports = {
     semi: ['error', 'never'],
     'no-console': 'off',
     'no-underscore-dangle': 'off',
-    'import/prefer-default-export': 'off',
-    'max-len': 'off',
     'import/extensions': 'off',
+    'import/no-unresolved': 'off',
+    'import/prefer-default-export': 'off',
+    'max-len': ['error', { code: 100 }],
+    'new-cap': 'off',
   },
 }
